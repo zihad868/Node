208687ea570994f55667e7757fb428f606cbd880
zihad868 <zihadh868@gmail.com>
2023-12-27 18:47:17 +0600

    Node File Read, Write

---
+++ File/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+// File Write 
+fs.writeFileSync('file.txt', 'Hello Bob. ')
+fs.appendFileSync('file.txt', 'How are you Alice?')
+
+
+// File Read 
+const data = fs.readFileSync('file.txt')
+
+console.log(data.toString())
---
+++ OS/index.js
@@ -0,0 +1,5 @@
+const os = require('node:os');
+
+console.log(os.cpus())
+
+console.log("Home Directory: ",os.homedir())
